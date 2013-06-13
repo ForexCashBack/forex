cd01139d91cde4561ea06195b187e2d934c4edb5
Jonathon Suggs <jsuggs@murmp.com>
2013-06-13 00:05:51 -0500

    Fixtues and some basic data

---
+++ src/Forex/Bundle/CoreBundle/DataFixtures/ORM/BrokerData.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Forex\Bundle\CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Forex\Bundle\CoreBundle\Entity\Broker;
+
+class BrokerData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $test = new Broker();
+        $test->setName('Test Broker');
+        $test->setCompanyName('Test Broker, LLC');
+        $test->setReferralLink('http://www.test.com?referral_link=test');
+        $test->setBasePercentage(80);
+        $test->setMinDeposit(50);
+        $test->setMaxLeverage(500);
+        $test->setWebsite('http://www.test.com');
+        $test->setYearFounded(1998);
+        $test->setHighlight('This is the test broker, it is great becase it is our first broker');
+        $manager->persist($test);
+
+        $fxpro = new Broker();
+        $fxpro->setName('FxPro');
+        $fxpro->setCompanyName('FxPro Financial Services Ltd');
+        $fxpro->setReferralLink('http://www.fxpro.com?referral_link=test');
+        $fxpro->setBasePercentage(80);
+        $fxpro->setMinDeposit(50);
+        $fxpro->setMaxLeverage(500);
+        $fxpro->setWebsite('http://www.fxpro.com');
+        $fxpro->setYearFounded(2008);
+        $fxpro->setHighlight('Hold accounts in CHF, EUR, GBP, JPY or USD. Tight spreads. 8 Platforms. FSA, ASIC, and CYSEC regulated.');
+        $manager->persist($fxpro);
+
+        $excel = new Broker();
+        $excel->setName('Excel Markets');
+        $excel->setCompanyName('Global Brokers NZ Ltd.');
+        $excel->setReferralLink('https://www.excelmarkets.com?referral_link=test');
+        $excel->setBasePercentage(80);
+        $excel->setMinDeposit(200);
+        $excel->setMaxLeverage(400);
+        $excel->setWebsite('https://www.excelmarkets.com/');
+        $excel->setYearFounded(2002);
+        $excel->setHighlight('Tight spreads with NO Deposit Fees. ECN/STP. MT4. 400:1 leverage. Micro lots.');
+        $manager->persist($excel);
+
+        $manager->flush();
+
+        $this->addReference('broker.test', $test);
+        $this->addReference('broker.fxpro', $fxpro);
+        $this->addReference('broker.excel', $excel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
---
+++ src/Forex/Bundle/CoreBundle/DataFixtures/ORM/RegulatorData.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Forex\Bundle\CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Forex\Bundle\CoreBundle\Entity\Regulator;
+use Forex\Bundle\CoreBundle\Entity\Regulation;
+
+class RegulatorData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $cysec = new Regulator();
+        $cysec->setAbbr('CYSEC');
+        $cysec->setName('Cyprus Securities and Exchange Commission');
+        $cysec->setUrl('http://www.cysec.gov.cy/licence_members_1_en.aspx');
+        $manager->persist($cysec);
+
+        $asic = new Regulator();
+        $asic->setAbbr('ASIC');
+        $asic->setName('Australian Securities & Investments Commission');
+        $asic->setUrl('http://www.asic.gov.au/');
+        $manager->persist($asic);
+
+        $fsa = new Regulator();
+        $fsa->setAbbr('FSA');
+        $fsa->setName('Financial Services Authority');
+        $fsa->setUrl('http://www.fsa.gov.uk/');
+        $manager->persist($fsa);
+
+        $fma = new Regulator();
+        $fma->setAbbr('FMA');
+        $fma->setName('Financial Markets Authority');
+        $fma->setUrl('http://www.fma.govt.nz/laws-we-enforce/registers/list-of-authorised-futures-dealers/');
+        $manager->persist($fma);
+
+        // Create the broker regulations
+
+        // Test Broker
+        $testCysec = new Regulation();
+        $testCysec->setBroker($this->getReference('broker.test'));
+        $testCysec->setRegulator($cysec);
+        $manager->persist($testCysec);
+
+        // FxPro
+        $fxproCysec = new Regulation();
+        $fxproCysec->setBroker($this->getReference('broker.fxpro'));
+        $fxproCysec->setRegulator($cysec);
+        $manager->persist($fxproCysec);
+
+        $fxproAsic = new Regulation();
+        $fxproAsic->setBroker($this->getReference('broker.fxpro'));
+        $fxproAsic->setRegulator($asic);
+        $manager->persist($fxproAsic);
+
+        $fxproFsa = new Regulation();
+        $fxproFsa->setBroker($this->getReference('broker.fxpro'));
+        $fxproFsa->setRegulator($fsa);
+        $manager->persist($fxproFsa);
+
+        // Excel
+        $excelCysec = new Regulation();
+        $excelCysec->setBroker($this->getReference('broker.excel'));
+        $excelCysec->setRegulator($cysec);
+        $manager->persist($excelCysec);
+
+        $excelAsic = new Regulation();
+        $excelAsic->setBroker($this->getReference('broker.excel'));
+        $excelAsic->setRegulator($asic);
+        $manager->persist($excelAsic);
+
+        $excelFsa = new Regulation();
+        $excelFsa->setBroker($this->getReference('broker.excel'));
+        $excelFsa->setRegulator($fsa);
+        $manager->persist($excelFsa);
+
+        // Add some regulations
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 2;
+    }
+}
