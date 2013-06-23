3908e3b419796ab13b5011e20eaadc08de896549
Jonathon Suggs <jsuggs@murmp.com>
2013-06-23 15:38:31 -0500

    Cashback estimator js

---
+++ web/js/cashback-estimator.js
@@ -0,0 +1,13 @@
+(function($) {
+    $(document).on('click', '#cb_est_calculate', function(e) {
+        e.preventDefault();
+
+        var volume = Number($("#cb_est_volume").val());
+        var trades = Number($("#cb_est_trades").val());
+        var rate = Number($("#cb_est_rate").val());
+        var base = volume * trades * rate;
+
+        $("#cs_est_monthly_earnings").html((base * 210).toFixed(2));
+        $("#cs_est_yearly_earnings").html((base * 2520).toFixed(2));
+    });
+})(jQuery);
