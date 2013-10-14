7329397a859a182c0d0ebe787387652e1d0c2f9e
Jonathon Suggs <jsuggs@murmp.com>
2013-10-13 21:16:02 -0500

    Admin stuffs

---
+++ src/Forex/Bundle/AdminBundle/Admin/EmailLinkAdmin.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Forex\Bundle\AdminBundle\Admin;
+
+use Forex\Bundle\CoreBundle\Entity\Account;
+use Forex\Bundle\CoreBundle\Entity\Broker;
+use Forex\Bundle\EmailBundle\Entity\EmailLink;
+use Forex\Bundle\EmailBundle\Entity\EmailMessage;
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class EmailLinkAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id', 'string')
+            ->add('toUrl')
+            ->add('message', 'sonata_type_model', array(), array())
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('toUrl')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('message')
+        ;
+    }
+
+    public function getNewInstance()
+    {
+        return new EmailLink('http://test.com', new EmailMessage('', ''));
+    }
+}
