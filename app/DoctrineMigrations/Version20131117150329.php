4f8195865b96e3f9ac98f4cb4567a4974a3c5daf
Jonathon Suggs <jsuggs@murmp.com>
2013-11-17 15:27:01 -0600

    Better index and fk names

--- app/DoctrineMigrations/Version20131117150329.php
+++ app/DoctrineMigrations/Version20131117150329.php
@@ -10,11 +10,11 @@ class Version20131117150329 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->addSql("CREATE TABLE broker_trading_platforms (platform VARCHAR(10) NOT NULL, accountType INT NOT NULL, PRIMARY KEY(accountType, platform))");
-        $this->addSql("CREATE INDEX IDX_29A345AC217CB8B3 ON broker_trading_platforms (accountType)");
-        $this->addSql("CREATE INDEX IDX_29A345AC3952D0CB ON broker_trading_platforms (platform)");
+        $this->addSql("CREATE INDEX IDX_BROKER_TRADING_PLATFORMS_ACCOUNT_TYPE ON broker_trading_platforms (accountType)");
+        $this->addSql("CREATE INDEX IDX_BROKER_TRADING_PLATFORMS_PLATFORM ON broker_trading_platforms (platform)");
         $this->addSql("CREATE TABLE trading_platforms (abbr VARCHAR(10) NOT NULL, description VARCHAR(100) NOT NULL, PRIMARY KEY(abbr))");
-        $this->addSql("ALTER TABLE broker_trading_platforms ADD CONSTRAINT FK_29A345AC217CB8B3 FOREIGN KEY (accountType) REFERENCES broker_account_types (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
-        $this->addSql("ALTER TABLE broker_trading_platforms ADD CONSTRAINT FK_29A345AC3952D0CB FOREIGN KEY (platform) REFERENCES trading_platforms (abbr) NOT DEFERRABLE INITIALLY IMMEDIATE");
+        $this->addSql("ALTER TABLE broker_trading_platforms ADD CONSTRAINT FK_BROKER_TRADING_PLATFORMS_REF_BROKER_ACCOUNT_TYPES_ACCOUNT_TYPE FOREIGN KEY (accountType) REFERENCES broker_account_types (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
+        $this->addSql("ALTER TABLE broker_trading_platforms ADD CONSTRAINT FK_BROKER_TRADING_PLATFORMS_REF_TRADING_PLATFORMS_PLATFORM FOREIGN KEY (platform) REFERENCES trading_platforms (abbr) NOT DEFERRABLE INITIALLY IMMEDIATE");
     }
 
     public function down(Schema $schema)
