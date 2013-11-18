0ad21a4c9c913d4a3d068d8b8440de4c128b87b0
Jonathon Suggs <jsuggs@murmp.com>
2013-11-17 23:43:03 -0600

    Function tests for help controller

--- src/Forex/Bundle/WebBundle/Tests/HelpControllerTest.php
+++ src/Forex/Bundle/WebBundle/Tests/HelpControllerTest.php
@@ -6,10 +6,42 @@ use Forex\Bundle\CoreBundle\Test\WebTestCase;
 
 class HelpControllerTest extends WebTestCase
 {
+    public function testFaq()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/help/faq');
+
+        $this->assertResponseSuccess($client->getResponse());
+    }
+
+    public function testGettingStarted()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/help/getting-started');
+
+        $this->assertResponseSuccess($client->getResponse());
+    }
+
     public function testHowToChooseABroker()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/en/help/how-to-choose-a-broker');
+
+        $this->assertResponseSuccess($client->getResponse());
+    }
+
+    public function testResidualIncode()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/help/residual-income');
+
+        $this->assertResponseSuccess($client->getResponse());
+    }
+
+    public function testEarningPotential()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/help/earning-potential');
 
         $this->assertResponseSuccess($client->getResponse());
     }
