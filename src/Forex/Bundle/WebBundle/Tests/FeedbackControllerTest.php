a8baf0346b7dabee7a77b16edceddad3f0b51e06
Jonathon Suggs <jsuggs@murmp.com>
2013-11-18 22:00:52 -0600

    FeedbackControllerTest

---
+++ src/Forex/Bundle/WebBundle/Tests/FeedbackControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Forex\Bundle\WebBundle\Tests;
+
+use Forex\Bundle\CoreBundle\Test\WebTestCase;
+
+class FeedbackControllerTest extends WebTestCase
+{
+    public function testBrokerSuggestion()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/feedback/suggestion');
+
+        $this->assertResponseSuccess($client->getResponse());
+    }
+
+    public function testComplaint()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/feedback/complaint');
+
+        $this->assertResponseSuccess($client->getResponse());
+    }
+}
