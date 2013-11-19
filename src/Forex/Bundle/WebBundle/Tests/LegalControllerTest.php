5e5335d641518ea64295e448412590eee3cea095
Jonathon Suggs <jsuggs@murmp.com>
2013-11-18 22:19:09 -0600

    LegalControllerTest

---
+++ src/Forex/Bundle/WebBundle/Tests/LegalControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Forex\Bundle\WebBundle\Tests;
+
+use Forex\Bundle\CoreBundle\Test\WebTestCase;
+
+class LegalControllerTest extends WebTestCase
+{
+    public function testPrivacyPolicy()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/legal/privacy-policy');
+
+        $this->assertResponseSuccess($client->getResponse());
+    }
+
+    public function testRegistrationAgreement()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/legal/registration-agreement');
+
+        $this->assertResponseSuccess($client->getResponse());
+    }
+
+    public function testRiskDisclosure()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/legal/risk-disclosure-statment');
+
+        $this->assertResponseSuccess($client->getResponse());
+    }
+}
